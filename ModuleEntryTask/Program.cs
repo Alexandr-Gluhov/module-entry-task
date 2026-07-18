c2a7673c72f1adc65dfda68139007e4bed6c9bc9
Alexander Gluhov <sanjok2505@gmail.com>
2026-07-19 01:42:04 +0300

    add healthcheck

--- ModuleEntryTask/Program.cs
+++ ModuleEntryTask/Program.cs
@@ -3,9 +3,12 @@ using ModuleEntryTask.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddApplicationDb();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
 await app.MigrateDbAsync();
+
+app.MapHealthChecks("/health");
 
 app.Run();
