88a8034aa2cda00afcad058fb3d9b035df9cd1ec
Alexander Gluhov <sanjok2505@gmail.com>
2026-07-19 21:28:39 +0300

    update migrations

--- ModuleEntryTask/Migrations/20260719181655_ReceiptAndEventsSupport.Designer.cs
+++ ModuleEntryTask/Migrations/20260719181655_ReceiptAndEventsSupport.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ModuleEntryTask.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ModuleEntryTask.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260719181655_ReceiptAndEventsSupport")]
+    partial class ReceiptAndEventsSupport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ModuleEntryTask/Migrations/20260719181655_ReceiptAndEventsSupport.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ModuleEntryTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReceiptAndEventsSupport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OperationId1",
+                table: "SubmitIntents",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ToStatus",
+                table: "OperationEvents",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubmitIntents_OperationId1",
+                table: "SubmitIntents",
+                column: "OperationId1",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SubmitIntents_Operations_OperationId1",
+                table: "SubmitIntents",
+                column: "OperationId1",
+                principalTable: "Operations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SubmitIntents_Operations_OperationId1",
+                table: "SubmitIntents");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SubmitIntents_OperationId1",
+                table: "SubmitIntents");
+
+            migrationBuilder.DropColumn(
+                name: "OperationId1",
+                table: "SubmitIntents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ToStatus",
+                table: "OperationEvents",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
